59e1bf94eaa141ec4ba5f7b0e8c6e68564899671
Mishig Davaadorj <dmishig@gmail.com>
2018-09-04 14:55:06 -0600

    chore: clear old files

--- description.js
+++
@@ -1,8 +0,0 @@
-import { markdown } from 'markdown';
-const fs = require("fs");
-
-const description = document.getElementById('description');
-var descriptionMarkdown = fs.readFileSync("./description.md", "utf-8");
-var desc = document.createElement('div');
-desc.innerHTML = markdown.toHTML(descriptionMarkdown);
-description.appendChild(desc);
